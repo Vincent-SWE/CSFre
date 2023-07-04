0f8f44deb66ecee6acfa9666a2025f4a10f23197
Vincent <VincentNSWE@gmail.com>
2023-07-04 09:39:42 +0800

    initial

---
+++ server/src/main/java/CSFre/CSFre/CsFreApplication.java
@@ -0,0 +1,13 @@
+package CSFre.CSFre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsFreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsFreApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/CSFre/CSFre/CsFreApplicationTests.java
@@ -0,0 +1,13 @@
+package CSFre.CSFre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsFreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
